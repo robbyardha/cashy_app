acba1bd3b6f2b676be8e2f21609395e54b37b79d
Robby Firmansyah Ardha <robbyardha@gmail.com>
2022-08-04 04:30:22 +0700

    new desain

---
+++ lib/font_style.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+TextStyle mainHeader = TextStyle(
+  fontSize: 26,
+  color: Colors.blue,
+  fontFamily: 'Poppins'
+);
+
+
+TextStyle subHeader = TextStyle(
+  fontSize: 16,
+  color: Colors.grey,
+  fontFamily: 'Poppins'
+);
